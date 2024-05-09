f551264351306fbf5cec9e34b64f98fb061bdb39
Farrukh <farrukh.jamal@bombaysoftwares.com>
2024-05-09 18:18:50 +0530

    refactor: merge branch prod

--- next.config.js
+++ next.config.js
@@ -1,14 +1,14 @@
 /** @type {import('next').NextConfig} */
-const path = require("path");
-const NODE_ENV = process.env.NODE_ENV;
-const withImages = require('next-images')
+const path = require("path")
+const NODE_ENV = process.env.NODE_ENV
+const withImages = require("next-images")
 
 module.exports = withImages({
     webpack5: false,
     reactStrictMode: true,
     generateEtags: false,
     sassOptions: {
-        includePaths: [path.join(__dirname, "styles")]
+        includePaths: [path.join(__dirname, "styles")],
     },
     eslint: {
         // Warning: This allows production builds to successfully complete even if
@@ -24,7 +24,7 @@ module.exports = withImages({
     },
     swcMinify: true,
     compiler: {
-        removeConsole: NODE_ENV === 'production' ? true : false,
+        removeConsole: NODE_ENV === "production" ? true : false,
     },
     compress: true,
     poweredByHeader: false,
@@ -32,37 +32,36 @@ module.exports = withImages({
         buildActivity: false,
     },
     env: {
-        APP_ENV: NODE_ENV
+        APP_ENV: NODE_ENV,
     },
     // Adding policies:
     async headers() {
         return [
             {
-              source: '/(.*)',
-              headers: [
-                {
-                  key: 'X-Frame-Options',
-                  value: 'DENY',
-                },
-                {
-                  key: 'Content-Security-Policy',
-                  value:
-                    "default-src 'self' 'https://bombaysoftwares.com'; image-src 'https://unsplash.com'; script-src 'self' https://www.google-analytics.com; font-src 'self' 'https://fonts.googleapis.com'",
-                },
-                {
-                  key: 'X-Content-Type-Options',
-                  value: 'nosniff',
-                },
-                // enable if required {
-                //   key: 'Permissions-Policy',
-                //   value: "camera=(); battery=(self); geolocation=(); microphone=('https://bombaysoftwares.com')",
-                // },
-                {
-                  key: 'Referrer-Policy',
-                  value: 'origin-when-cross-origin',
-                },
-              ],
+                source: "/(.*)",
+                headers: [
+                    {
+                        key: "X-Frame-Options",
+                        value: "DENY",
+                    },
+                    {
+                        key: "Content-Security-Policy",
+                        value: "default-src 'self' 'https://bombaysoftwares.com'; image-src 'https://unsplash.com'; script-src 'self' https://www.google-analytics.com; font-src 'self' 'https://fonts.googleapis.com'",
+                    },
+                    {
+                        key: "X-Content-Type-Options",
+                        value: "nosniff",
+                    },
+                    // enable if required {
+                    //   key: 'Permissions-Policy',
+                    //   value: "camera=(); battery=(self); geolocation=(); microphone=('https://bombaysoftwares.com')",
+                    // },
+                    {
+                        key: "Referrer-Policy",
+                        value: "origin-when-cross-origin",
+                    },
+                ],
             },
-          ];
-    }
-});
+        ]
+    },
+})

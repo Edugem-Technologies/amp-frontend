cd00ac81dbac9de50a118c9ef52480501d82b538
Farrukh <farrukh.jamal@bombaysoftwares.com>
2024-12-25 15:38:17 +0530

    refactor: name updated

--- src/app/components/demo/IntersectionObserver.tsx
+++ src/app/components/demo/IntersectionObserver.tsx
@@ -4,10 +4,10 @@ import { useRef } from "react"
 
 const IntersectionObserver = () => {
     const ref = useRef<HTMLParagraphElement | null>(null)
-    const isInView = useIntersectionObserver(ref, { threshold: 0, rootMargin: "-60px" })
+    const isInView = useIntersectionObserver(ref, { threshold: 0.5, rootMargin: "-60px" })
     return (
         <div>
-            <h2>IntersectionObserver</h2>
+            <h2>useIntersectionObserver hook</h2>
             <p ref={ref} className={`m-0 ${isInView ? "text-danger" : ""}`}>
                 This text turns to red when it is about to be visible on the screen
             </p>
--- src/app/components/demo/Scroll.tsx
+++ src/app/components/demo/Scroll.tsx
@@ -5,7 +5,7 @@ const Scroll = () => {
     const position = useScroll()
     return (
         <div>
-            <h2>Scroll Position</h2>
+            <h2>useScroll hook</h2>
             <p className="m-0">Scroll position is: {position}</p>
         </div>
     )

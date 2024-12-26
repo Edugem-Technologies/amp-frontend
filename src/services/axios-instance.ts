f70558e33404b2426afdef41524f357ec56ad574
Farrukh <farrukh.jamal@bombaysoftwares.com>
2024-12-26 10:43:29 +0530

    refactor: ngrok header added

--- src/services/axios-instance.ts
+++ src/services/axios-instance.ts
@@ -9,6 +9,7 @@ import axios from "axios"
 export const axiosInstance = axios.create({
     headers: {
         "Content-Type": "application/json",
+        "ngrok-skip-browser-warning": "true",
     },
 })
 

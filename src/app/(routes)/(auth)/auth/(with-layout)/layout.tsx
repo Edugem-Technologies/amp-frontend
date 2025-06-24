b826ab0b791c0e5c8c54b34ace15b7254e0af3a8
GouravVijaywargiya26 <gourav.vijaywargiya@bombaysoftwares.com>
2025-06-24 13:14:54 +0530

    style: updated auth form styles

--- src/app/(routes)/(auth)/auth/(with-layout)/layout.tsx
+++ src/app/(routes)/(auth)/auth/(with-layout)/layout.tsx
@@ -18,8 +18,8 @@ const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
             <Toaster position="top-center" />
             <div className="sign-in-layout w-lg-500px p-10 m-auto">
                 <LoginSwiperSection />
-                <div className="w-100 container flex-center sign-in-form">
-                    <div className={`${isSignUpPage ? "h-100 sign-up-form" : "w-500px"}`}>
+                <div className="w-100 h-100 container flex-center sign-in-form">
+                    <div className={`${isSignUpPage ? "sign-up-form" : "w-500px"}`}>
                         <img
                             className="logo-image"
                             src="/images/logos/logo.png"

46127b2de2a4b377311a981356ac5a74fe082d02
GouravVijaywargiya26 <gourav.vijaywargiya@bombaysoftwares.com>
2025-06-27 12:06:36 +0530

    feat: update details api endpoint on google success page

--- src/app/(routes)/(auth)/auth/(fallback)/google/login/success/page.tsx
+++ src/app/(routes)/(auth)/auth/(fallback)/google/login/success/page.tsx
@@ -5,22 +5,24 @@ import { ALERT_ICON_TYPE, CONFIG } from "@/utils/constants"
 import { handleError } from "@/utils/handle-error"
 import { setLoginDetailsToLocalStorage, showSweetAlertWithRedirect } from "@/utils/helpers"
 import { useRouter, useSearchParams } from "next/navigation"
-import React, { useEffect } from "react"
+import { useEffect } from "react"
 
 const Page = () => {
     const { setUserPermissions } = usePermissions()
     const router = useRouter()
     const searchParams = useSearchParams()
     const message = searchParams.get(`message`)
+    const user_uuid = searchParams.get(`user_uuid`)
     const getUserDetails = async () => {
         try {
-            const response = await FetchHelper.get(CONFIG.API_ENDPOINTS.GET_USER_DETAILS)
+            const url = new URL(`${CONFIG.API_ENDPOINTS.BASE_USER}/${user_uuid}/details`)
+            const response = await FetchHelper.get(url)
             if (response?.status) {
                 setLoginDetailsToLocalStorage({
                     permissions: response.data.permissions,
                     roles: response.data.roles,
                     setUserPermissions,
-                    userDetails: response.data.user,
+                    userDetails: response.data,
                 })
             }
         } catch (error) {

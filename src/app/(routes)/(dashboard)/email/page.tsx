6da80b059af197d3e485bf7f3893a2b1db2423e2
BS Ashish <ashish.kumar@bombaysoftwares.com>
2026-05-12 17:47:19 +0530

    feat: dashboard page

---
+++ src/app/(routes)/(dashboard)/email/page.tsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+const page = () => {
+    return (
+        <div className="content d-flex flex-column flex-column-fluid">
+            <div className="post d-flex flex-column-fluid">
+                <div className="container-fluid">
+                    <div className="g-5 gx-xxl-8">Email features will be displayed here.</div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default page
